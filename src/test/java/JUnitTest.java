d98b572498f3e4b96bd32177e38a6ce980bcccc7
DanVu <danvu@ya.ru>
2021-08-31 18:07:31 +0300

    reformat all code

--- src/test/java/JUnitTest.java
+++ src/test/java/JUnitTest.java
@@ -1,8 +1,4 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class JUnitTest {
 
