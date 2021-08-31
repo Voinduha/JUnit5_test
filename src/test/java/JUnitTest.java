5661a7604d76bd84bc7c663b03ed41a4e6bdca65
DanVu <danvu@ya.ru>
2021-08-31 17:59:25 +0300

    Added README file

--- src/test/java/JUnitTest.java
+++ src/test/java/JUnitTest.java
@@ -8,28 +8,28 @@ public class JUnitTest {
 
     @BeforeAll
     static void beforeAll() {
-        System.out.println("this is before all method!");
+        System.out.println("This is before all method!");
     }
 
     @BeforeEach
     void setUp() {
-        System.out.println("this is before each method!");
+        System.out.println("This is before each method!");
     }
 
     @AfterEach
     void tearDown() {
-        System.out.println("this is after each method!");
+        System.out.println("This is after each method!");
     }
 
     @Test
     void firstTest() {
-        System.out.println("this is the first @test!");
+        System.out.println("This is the first @test!");
         Assertions.assertTrue(true);
     }
 
     @Test
     void secondTest() {
-        System.out.println("this is the second @test!");
+        System.out.println("This is the second @test!");
         Assertions.assertTrue(true);
     }
 }
